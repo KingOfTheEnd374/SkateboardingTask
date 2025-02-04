0bce3e1743f3331acd9b5269fac718fa1c07caf0
KingOfTheEnd374 <torn.remo@gmail.com>
2025-02-04 17:59:07 +0200

    Basic obstacle

--- Source/Skateboarding/Private/Obstacle/Obstacle.cpp
+++ Source/Skateboarding/Private/Obstacle/Obstacle.cpp
@@ -2,13 +2,26 @@
 
 
 #include "Obstacle/Obstacle.h"
+#include <Player/SkateboardingPawn.h>
+#include <Player/SkateboardingPlayerController.h>
 
 // Sets default values
 AObstacle::AObstacle()
 {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
+	PrimaryActorTick.bCanEverTick = false;
 
+	Mesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Mesh"));
+	SetRootComponent(Mesh);
+	Mesh->SetCollisionProfileName(TEXT("BlockAll"));
+	Mesh->SetGenerateOverlapEvents(true);
+
+	JumpingOverCollision = CreateDefaultSubobject<UBoxComponent>(TEXT("JumpingOverCollision"));
+	JumpingOverCollision->SetupAttachment(Mesh);
+	JumpingOverCollision->SetGenerateOverlapEvents(true);
+	JumpingOverCollision->SetCollisionObjectType(ECC_WorldDynamic);
+	JumpingOverCollision->SetCollisionResponseToAllChannels(ECR_Ignore);
+	JumpingOverCollision->SetCollisionResponseToChannel(ECC_Pawn, ECR_Overlap);
 }
 
 // Called when the game starts or when spawned
@@ -16,6 +29,17 @@ void AObstacle::BeginPlay()
 {
 	Super::BeginPlay();
 	
+	JumpingOverCollision->OnComponentBeginOverlap.AddDynamic(this, &AObstacle::BeginOverlap);
+}
+
+void AObstacle::BeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Yellow, TEXT("Overlap"));
+	ASkateboardingPawn* Player = Cast<ASkateboardingPawn>(OtherActor);
+	if (Player)
+	{
+		Cast<ASkateboardingPlayerController>(Player->GetController())->Score++;
+	}
 }
 
 // Called every frame
--- Source/Skateboarding/Public/Obstacle/Obstacle.h
+++ Source/Skateboarding/Public/Obstacle/Obstacle.h
@@ -4,20 +4,31 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Actor.h"
+#include <Components/BoxComponent.h>
 #include "Obstacle.generated.h"
 
 UCLASS()
 class SKATEBOARDING_API AObstacle : public AActor
 {
 	GENERATED_BODY()
-	
+
 public:	
 	// Sets default values for this actor's properties
 	AObstacle();
 
+	UPROPERTY(BlueprintReadOnly, EditDefaultsOnly)
+	UStaticMeshComponent* Mesh;
+
+	UPROPERTY(BlueprintReadOnly, EditDefaultsOnly)
+	UBoxComponent* JumpingOverCollision;
+
+
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
+
+	UFUNCTION()
+	void BeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult &SweepResult);
 
 public:	
 	// Called every frame
